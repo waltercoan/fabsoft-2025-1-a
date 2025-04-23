0fa3045ab391618aee1b532f09e0f1a85c4ed3fd
WALTER SILVESTRE COAN <walter.s@univille.br>
2025-04-22 22:05:46 -0300

    post e put

--- projfabsoft/src/main/java/br/univille/projfabsoft/controller/ClienteController.java
+++ projfabsoft/src/main/java/br/univille/projfabsoft/controller/ClienteController.java
@@ -6,6 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,4 +30,39 @@ public class ClienteController {
         return new ResponseEntity<List<Cliente>>(listaClientes, 
                     HttpStatus.OK);
     }
+    @PostMapping
+    public ResponseEntity<Cliente> 
+            postCliente(@RequestBody Cliente cliente){
+        if(cliente == null){
+            return ResponseEntity.badRequest().build();
+        }
+        if(cliente.getId() == 0){
+            service.save(cliente);
+            return new ResponseEntity<Cliente>(cliente, HttpStatus.OK);
+        }
+        return ResponseEntity.badRequest().build();
+    }
+    @PutMapping("/{id}")
+    public ResponseEntity<Cliente> 
+        putCliente(@PathVariable long id,
+                   @RequestBody Cliente cliente){
+        if(id <= 0 || cliente == null){
+            return ResponseEntity.badRequest().build();
+        }
+        var clienteAntigo = service.getById(id);
+        if(clienteAntigo == null){
+            return ResponseEntity.notFound().build();
+        }
+
+        clienteAntigo.setNome(cliente.getNome());
+        clienteAntigo.setEndereco(cliente.getEndereco());
+        clienteAntigo.setTelefone(cliente.getTelefone());
+        clienteAntigo.setEmail(cliente.getEmail());
+        clienteAntigo.setDataNascimento(cliente.getDataNascimento());
+     
+        service.save(clienteAntigo);
+        return new ResponseEntity<Cliente>(clienteAntigo, HttpStatus.OK);
+    }
+
+
 }
