14af42d3c3cb6f42947782008b200330db7bc375
Walter Silvestre Coan <walter.coan@gmail.com>
2025-05-28 22:09:03 +0000

    WIP: tela clientes

--- projfabsoft_frontend/src/app/cliente/cliente.component.ts
+++ projfabsoft_frontend/src/app/cliente/cliente.component.ts
@@ -1,16 +1,20 @@
 import { Component } from '@angular/core';
 import { Cliente } from '../model/cliente';
 import { ClienteService } from '../service/cliente.service';
+import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-cliente',
-  imports: [],
+  imports: [HttpClientModule, CommonModule],
   templateUrl: './cliente.component.html',
-  styleUrl: './cliente.component.css'
+  styleUrl: './cliente.component.css',
+  providers: [ClienteService]
 })
 export class ClienteComponent {
 
     public listaClientes:Cliente[] = [];
+    
     constructor(
       private clienteService:ClienteService
     ){}
