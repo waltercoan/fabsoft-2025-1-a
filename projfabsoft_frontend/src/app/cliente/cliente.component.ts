a90f5ef3c6d8846301b6296f9c87c7d788b48b99
Walter Silvestre Coan <walter.coan@gmail.com>
2025-06-11 00:23:34 +0000

    correcao novo

--- projfabsoft_frontend/src/app/cliente/cliente.component.ts
+++ projfabsoft_frontend/src/app/cliente/cliente.component.ts
@@ -3,27 +3,30 @@ import { Cliente } from '../model/cliente';
 import { ClienteService } from '../service/cliente.service';
 import { HttpClientModule } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
-import { RouterLink } from '@angular/router';
-
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-cliente',
-  imports: [HttpClientModule, CommonModule, RouterLink],
+  imports: [HttpClientModule, CommonModule],
   templateUrl: './cliente.component.html',
   styleUrl: './cliente.component.css',
-  providers: [ClienteService]
+  providers: [ClienteService, Router]
 })
 export class ClienteComponent {
 
     public listaClientes:Cliente[] = [];
     
     constructor(
-      private clienteService:ClienteService
+      private clienteService:ClienteService,
+      private router:Router
     ){}
 
     ngOnInit(): void {
       this.clienteService.getClientes().subscribe(resposta => {
           this.listaClientes = resposta;
       })
+    }
+    novo(){
+      this.router.navigate(['clientes/novo']);
     }
 }
